ca85ddaf945b4b16bf1a6b65a7428a24f9e98f1e
jigius <jigius@gmail.com>
2023-08-24 12:13:44 +0700

    adds langs files for RU

---
+++ resources/lang/en/feedback.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "message" => "Message",
+    "email" => "Email",
+    "farewell" => "Thank You for the feedback!",
+    "validation_is_failure" => "Something went wrong!"
+];
---
+++ resources/lang/ru/feedback.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "message" => "Сообщение",
+    "email" => "Email",
+    "farewell" => "Спасибо за ваш отзыв!",
+    "validation_is_failure" => "Что-то пошло не так!"
+];
